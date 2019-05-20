ef2606b2b76befa80945afb33d518fb3dc1b3f04
Kainoad <kaid@beaverisland.k12.mi.us>
2019-05-20 14:40:48 -0400

    Chvt is installed by default on a pi

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #Automated Sprinkler Shutoff System setup script
 
-apt-get install chvt
 cp sprinkler.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable sprinkler.service
